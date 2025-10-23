c4fe25af7af6890d9521ab5426364621df7b657b
Adriano <a>
2025-10-23 19:49:44 -0300

    update

---
+++ autoload.php
@@ -0,0 +1,19 @@
+<?php
+spl_autoload_register(function ($class) {
+    // Caminho base do projeto (raiz)
+    $baseDir = __DIR__ . '/';
+
+    // Mapeia namespaces ou pastas simples
+    $paths = [
+        $baseDir . 'class/',     // pasta das classes
+        $baseDir . 'app/',       // se tiver classes dentro de app/
+    ];
+
+    foreach ($paths as $path) {
+        $file = $path . $class . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
+    }
+});
--- teste.php
+++ teste.php
@@ -1,18 +1,58 @@
 <?php
+ini_set('display_errors', 1);
 
-require_once __DIR__ . '/htdocs/app/models/DataBase.php';
-require_once __DIR__ . '/htdocs/app/models/User.php';
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/autoload.php';
+
+//require_once __DIR__ . '/models/UserPdo.php';
 
 //session_start();
 
-/*$db = new DataBase();
+/**/$db = new DataBase();
+$db->getPdo()->exec("
+
+            CREATE TABLE IF NOT EXISTS users (
+
+                id INT AUTO_INCREMENT PRIMARY KEY,
+
+                nome VARCHAR(100) NOT NULL,
+
+                email VARCHAR(150) UNIQUE NOT NULL,
+
+                senha VARCHAR(255) NOT NULL,
+
+                criado_em DATETIME NOT NULL
+
+            );
+
+        ");
+/**/
+$novo = new User();
+$novo->nome="Adriiano";
+//$udb= new UserPdo();
+//$udb->save($novo);
+echo __DIR__;
+
+
+
+
+
 
 $stmt = $db->query("SELECT * FROM users LIMIT 1");
 $data = $stmt->fetch(PDO::FETCH_ASSOC);
-$user =User::createFromArray($data);
+//$user =User::createFromArray($data);
 
-echo $user->nome;
-*/
-echo "ok";
+
+echo $data;
+
+/**/
+echo "<br/>ok";
+
+
+
+
 
 ?>
