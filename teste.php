0a829107f5532eeb71b3d06c7c69a478f3006ce6
Adriano <a>
2025-10-23 18:44:06 -0300

    update teste.php

---
+++ teste.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/htdocs/app/models/DataBase.php';
+require_once __DIR__ . '/htdocs/app/models/User.php';
+
+//session_start();
+
+$db = new DataBase();
+
+$stmt = $db->query("SELECT * FROM users LIMIT 1");
+$data = $stmt->fetch(PDO::FETCH_ASSOC);
+$user =User::createFromArray($data);
+
+echo $user->nome;
+
+
+?>
