035979f401b6190ca78c14a3dc29b7fb5c18eff3
Adriano <a>
2025-10-24 01:12:44 -0300

    update games.php

--- painel/games.php
+++ painel/games.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Games</title>
+<?php include __DIR__.'/includes/header.php'; ?>
+<?php include __DIR__.'/includes/menu.php'; ?>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
 <style>
 .card {
@@ -209,5 +203,7 @@ function playGame(url) { if(url) window.open(url, '_blank'); else alert('URL do 
 
 $(function(){ loadGames(); });
 </script>
-</body>
-</html>
+<?php include __DIR__.'/includes/footer.php'; ?>
+
+
+
